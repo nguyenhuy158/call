d2e3bd008d3c448df9241eb8484f1c949b01e5cc
huy.ntq <hyquaq@gmail.com>
2023-11-19 23:45:30 +0700

    Fix bug in login functionality

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+io.on('connection', (socket) => {
+    // Handle signaling logic here
+    console.log('User connected');
+    console.log(socket.id);
+    
+    socket.on('offer', (data) => {
+        io.to(data.target).emit('offer', {target: socket.id, offer: data.offer});
+    });
+
+    socket.on('answer', (data) => {
+        io.to(data.target).emit('answer', data.answer);
+    });
+
+    socket.on('ice-candidate', (data) => {
+        io.to(data.target).emit('ice-candidate', data.candidate);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
